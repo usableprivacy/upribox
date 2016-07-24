0d45502e0bd0cf92cc524907ff4408c0b50485c2
Tobias Dam <is131003@fhstp.ac.at>
2016-07-24 20:23:04 +0200

    - bugfix: add_device would add enabled device, if device is disabled - added redis-server config to enable keyspace-events for expiring key

--- roles/arp/files/apate/lib/apate_redis.py
+++ roles/arp/files/apate/lib/apate_redis.py
@@ -16,20 +16,24 @@ class ApateRedis(object):
         self.network = network
         self.logger = logger
 
-    def add_device(self, ip, mac, network=None, enabled=True):
-        self._add_device_to_network(ip, network or self.network.network)
-        return self._add_entry(self._get_device_name(ip, network or self.network.network, enabled=enabled), mac)
+    def add_device(self, ip, mac, network=None, enabled=True, force=False):
+        if not self._check_device_disabled(ip, network or self.network.network) or force:
+            self._add_device_to_network(ip, network or self.network.network)
+            return self._add_entry(self._get_device_name(ip, network or self.network.network, enabled=enabled), mac)
 
     def remove_device(self, ip, network=None, enabled=True):
         self._del_device_from_network(ip, network or self.network.network)
         return self._del_device(self._get_device_name(ip, network or self.network.network, enabled=enabled))
+
+    def get_device_mac(self, ip, network=None, enabled=True):
+        return self.redis.get(self._get_device_name(ip, network or self.network.network, enabled=enabled))
 
     def get_devices(self, network=None):
         return self.redis.smembers(self._get_network_name(network or self.network.network))
 
     def get_devices_values(self, filter=False, network=None, enabled=True):
         # list may contain null values
-        devs = self.get_devices(network=network)
+        devs = self.get_devices(network=network or self.network.network)
         if not devs:
             return []
         else:
@@ -60,5 +64,19 @@ class ApateRedis(object):
     def _del_device_from_network(self, ip, network):
         return self.redis.srem(ApateRedis.__DELIMITER.join((ApateRedis.__PREFIX, ApateRedis.__NETWORK, str(network))), str(ip))
 
+    # True if disabled
+    def _check_device_disabled(self, ip, network):
+        return self.redis.get(self._get_device_name(ip, network, enabled=False)) is not None
+
     def get_pubsub(self, ignore_subscribe_messages=True):
         return self.redis.pubsub(ignore_subscribe_messages=ignore_subscribe_messages)
+
+    def disable_device(self, ip, network=None):
+        self._toggle_device(ip, network or self.network.network, enabled=False)
+
+    def enable_device(self, ip, network=None):
+        self._toggle_device(ip, network or self.network.network, enabled=True)
+
+    def _toggle_device(self, ip, network, enabled):
+        self.add_device(ip, self.get_device_mac(ip, network, enabled=not enabled), network, enabled=enabled, force=True)
+        self.remove_device(ip, network, enabled=not enabled)
