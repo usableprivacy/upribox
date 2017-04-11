d5992e2473e7c8073d5a4a7a2506bc435249b444
Julian Schrittwieser <schrittwieser@me.com>
2017-04-11 15:41:03 +0200

    - adapted fingerprinting script to changes in model

--- upribox_interface/devices/management/commands/fingerprint.py
+++ upribox_interface/devices/management/commands/fingerprint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 from django.core.management.base import NoArgsCommand
-from devices.models import DeviceEntry, DeviceName, UserAgent
+from devices.models import DeviceEntry
 from user_agents import parse
 
 #
@@ -11,15 +11,13 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         print "Starting fingerprinting devices..."
         for device in DeviceEntry.objects.all().iterator():
-            print "Fingerprinting device with IP " + device.ip + " ...\n"
-            for ua_string in device.user_agent.all().iterator():
-                print "Parsing User-Agent \"" + ua_string + "\" ..."
-                user_agent = parse(ua_string)
-                print "   Browser: " + user_agent.browser + "\n    OS: " + user_agent.os + "\n   Device: " + user_agent.device + "\n"
+            print "Fingerprinting device with IP/MAC " + device.ip + "/" + device.mac + " ...\n"
+            for ua_entry in device.user_agent.all().iterator():
+                print "Parsing User-Agent \"" + ua_entry.agent + "\" ..."
+                user_agent = parse(ua_entry.agent)
+                print "   Browser: " + str(user_agent.browser) + "\n   OS: " + str(user_agent.os) + "\n   Device: " + str(user_agent.device) + "\n"
                 model = user_agent.device.model
-                if device != "Other":
-                    device_name = DeviceName(name=model)
-                    device_name.save()
-                    device.add(device_name)
+                if device:
+                    ua_entry.model = model
                 else:
                     print "Ignore this device name ..."
