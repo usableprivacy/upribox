6caebbf5025a94eb78873a68b63a66ff95857d05
Tobias Dam <is131003@fhstp.ac.at>
2017-06-08 10:03:05 +0200

    changes for #64

---
+++ roles/wlan/templates/ifup_wlan.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+#pgrep ansible
+
+if [ ! -f /var/run/wlan.lock ]; then
+    touch /var/run/wlan.lock
+    case $IFACE in
+        *wlan*) /usr/local/bin/ansible-playbook -i {{ pull_workdir }}/environments/{{ "development" if env == "development" else "production" }}/inventory_pull -c local {{ pull_workdir }}/local.yml --tags wlan_device ;;
+    esac
+    rm /var/run/wlan.lock
+fi
+
+exit 0
