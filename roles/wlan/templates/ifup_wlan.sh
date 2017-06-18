29e67abef459ad8c5e9e29d9cf7fd3d9b87780d2
Tobias Dam <is131003@fhstp.ac.at>
2017-06-18 20:09:33 +0200

    minor changes

--- roles/wlan/templates/ifup_wlan.sh
+++ roles/wlan/templates/ifup_wlan.sh
@@ -5,7 +5,7 @@
 if [ ! -f /var/run/wlan.lock ]; then
     touch /var/run/wlan.lock
     case $IFACE in
-        *wlan*) nohup /usr/local/bin/ansible-playbook -i {{ pull_workdir }}/environments/{{ "development" if env == "development" else "production" }}/inventory_pull -c local {{ pull_workdir }}/local.yml --tags wlan_device &> /home/upri/ifup.log & ;;
+        *wlan*) setsid nohup setsid nohup /usr/bin/python -u /usr/local/bin/ansible-playbook -i {{ pull_workdir }}/environments/{{ "development" if env == "development" else "production" }}/inventory_pull -c local {{ pull_workdir }}/local.yml --tags wlan_device -v  >> /home/upri/ifup.log 2>&1 & ;;
     esac
     rm /var/run/wlan.lock
 fi
