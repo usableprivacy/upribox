387448f7c3dc5c308dff80aa620cdb86bccb3e3b
Tobias Dam <is131003@fhstp.ac.at>
2017-06-30 13:06:08 +0200

    - modified cron update schedule - added several cron jobs

--- roles/common/files/upri-filter-update.py
+++ roles/common/files/upri-filter-update.py
@@ -6,7 +6,8 @@ import requests
 from requests.exceptions import ConnectionError, Timeout, SSLError
 import hashlib
 import subprocess
-import sys, traceback
+import sys
+import traceback
 
 # path to the json file that contains the urls from where updates are loaded
 UPDATE_INFO_FILE = "/usr/local/etc/upri-filter-update/updates.json"
@@ -24,6 +25,8 @@ RESTART_WHITELIST = {
     "restart-nginx": ["/usr/sbin/service", "nginx", "reload"],
     "generate-tor-blocklist": ["/bin/bash", "-c", "/bin/sed 's/\.55\./\.56\./' /etc/dnsmasq.d/dnsblacklist.conf > /etc/dnsmasq.d.ninja/dnsblacklist.conf"]
 }
+
+
 def download_file(url):
 
     attempts = 0
@@ -37,7 +40,8 @@ def download_file(url):
             print "error getting %s: %s " % (url, e.message)
             attempts += 1
             if attempts >= MAX_ATTEMPTS:
-                raise e 
+                raise e
+
 
 def hash_file(path):
     if not os.path.isfile(path):
@@ -47,26 +51,27 @@ def hash_file(path):
         hsh.update(f.read())
         return hsh.hexdigest()
 
+
 def main():
     if os.path.isfile(UPDATE_INFO_FILE):
-        
+
         exit_code = 0
         to_restart = list()
-        
-        with open(UPDATE_INFO_FILE,'r') as f:
+
+        with open(UPDATE_INFO_FILE, 'r') as f:
             updates = json.load(f)
-        
+
         for update in updates:
             update_from = update.get('update_from', None)
             update_to = update.get('update_to', None)
             restart = update.get('run', [])
             sha256 = hash_file(update_to)
-            
+
             print "checking updates for %s" % update_to
-            
+
             # download current sha
             newsha256 = download_file(update_from + UPDATE_URL_POSTFIX)
-            
+
             # check if update is required
             if not sha256 or sha256 != newsha256:
                 exit_code = 1
@@ -76,17 +81,17 @@ def main():
                 # write new file
                 if not os.path.exists(os.path.dirname(update_to)):
                     os.makedirs(os.path.dirname(update_to))
-                
+
                 with open(update_to, 'w') as outf:
                     outf.write(updated_file)
-                
+
                 for service in restart:
                     if service not in to_restart:
                         to_restart.append(service)
 
         if(len(to_restart) > 0):
             print "updated complete, restarting services..."
-        
+
         for restart in to_restart:
             if restart and restart in RESTART_WHITELIST:
                 print "executing %s" % (restart)
