9db7e4b2f732cce20bd3e43f9aa34245484e6a1e
Julian Schrittwieser <schrittwieser@me.com>
2017-07-14 10:12:17 +0200

    - readthedocs does not find argparser.py. Implemented hacky solution by copying the file to the docs when executing make file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,12 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
-sys.path.insert(0, os.path.abspath('../roles/django/files/'))
+#import sys
+#sys.path.insert(0, os.path.abspath('../roles/django/files/'))
+path_to_argparser = '../../roles/django/files/argparser.py'
+if os.path.isfile(path_to_argparser):
+    from shutil import copyfile
+    copyfile(path_to_argparser, '../argparser.py')
 
 # -- General configuration ------------------------------------------------
 
