31a7ae90cea2abe6537b1734cf6816161de45087
Julian Schrittwieser <schrittwieser@me.com>
2017-07-14 09:09:26 +0200

    - fixed path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../roles/django/files/'))
+sys.path.insert(0, os.path.abspath('../roles/django/files/'))
 
 # -- General configuration ------------------------------------------------
 
