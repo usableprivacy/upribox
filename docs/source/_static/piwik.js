c05d8896ca1e8ebc434d6e7c691074fadecd3e18
Markus Donko-Huber <markus@nysos.net>
2018-06-13 22:36:55 +0200

    Minor fixes for documentation

--- docs/source/_static/piwik.js
+++ docs/source/_static/piwik.js
@@ -1,5 +1,3 @@
-<!-- Piwik -->
-<script type="text/javascript">
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setCookieDomain", "*.upribox.org"]);
@@ -12,6 +10,3 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<noscript><p><img src="//piwik.nysos.net/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
