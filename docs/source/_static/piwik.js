ae5ef844f50f2c028bdc9305d7fb2905ad774a45
Markus Donko-Huber <markus@nysos.net>
2019-05-17 12:54:21 +0200

    Update docs from piwik to matomo

--- docs/source/_static/piwik.js
+++ docs/source/_static/piwik.js
@@ -1,12 +1,13 @@
-  var _paq = _paq || [];
+var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setCookieDomain", "*.upribox.org"]);
+  _paq.push(["setDomains", ["*.upribox.org","*.docs.upribox.org"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//piwik.nysos.net/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    var u="https://matomo.nysos.net/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
